f84aae152fc74aa80795d71675825b2bd7d2368b
Ariana <ariaguirre64@gmail.com>
2022-08-22 20:19:20 -0300

    homework

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,65 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null; 
+}
+
+BinarySearchTree.prototype.size = function(){
+  if (this.value === null) return 0;
+  if (this.left === null && this.right === null) return 1;
+  if(this.left === null && this.right !== null) return 1 + this.right.size();    // 1 por el nodo y recursion
+  if(this.right === null && this.left !== null) return 1 + this.left.size();    // 1 por el nodo y recursion
+  return 1 + this.right.size() + this.left.size();    // recursion a la derecha y a la izquierda 
+};
+
+BinarySearchTree.prototype.insert = function(value){
+  if (value > this.value) {
+    if (this.right === null) this.right = new BinarySearchTree(value);
+    else this.right.insert(value);
+  }  
+    if (value > this.value) {
+    if (this.left === null) this.left = new BinarySearchTree(value);
+    else this.left.insert(value);
+  }
+};
+BinarySearchTree.prototype.contains = function(value){
+  if(this.value === value) return true;
+  if (value > this.value) {
+    if (this.right === null) return false;   
+    return this.right.contains(value);       
+  }
+  if (value < this.value) {
+    if (this.left === null) return false;    // si esta vacio retornar falso
+    return this.left.contains(value);       //si no esta vacio --> recurison
+  }
+};
+BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
+  if (orden === 'post-order') {
+
+  }
+  else if (orden === 'pre-order') {
+    cb(this.value);
+    if (this.left !== null) this.left.depthFirstForEach(cb, orden)
+    if (this.right !== null) this.right.depthFirstForEach(cb, orden)
+  }
+  else {
+    if (this.left !== null) this.left.depthFirstForEach(cb, orden)
+    cb(this.value);
+    if (this.right !== null) this.right.depthFirstForEach(cb, orden)
+  }
+};
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, queue = []){
+  if (this.left !== null) queue.push(this.left);
+  if (this.right !== null) queue.push(this.right);
+  cb(this.value);
+  if(queue.length > 0) {
+    queue.shift().breadthFirstForEach(cb, queue);
+  } 
+  }
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
